fe02b477e4bc67934b0f7053c820c35215981542
Daniel Felipe Yepes Ortega <danielfyo@MacBook-Pro-de-Daniel.local>
2022-02-08 20:01:53 -0500

    Inappropriate Information: Any comment with information that is better registered in another system must be eliminated and your information transferred to said system.

---
+++ src/Comments/InappropriateInformationTest.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+using Xunit.Categories;
+
+namespace CodeSmells
+{
+    /// <summary>
+    /// Any comment with information that is better registered in another system must be eliminated and your information transferred to said system.
+    /// </summary>
+    public class InappropriateInformationTest
+    {
+        /// <summary>
+        /// The author and the date of creation are information that belongs to the version control system
+        /// </summary>
+        [Fact]
+        [UnitTest]
+        public void Inappropriate_information_code_smell()
+        {
+            //Author Daniel Yepes
+            //Create Date 01-01-2001 01:01:01
+            string AnyVar = "new variable";
+        }
+
+        /// <summary>
+        /// Solution: remove comments and access information from the version control system
+        /// </summary>
+        [Fact]
+        [UnitTest]
+        public void Inappropriate_information_solution()
+        {
+            string AnyVar = "new variable";
+        }
+    }
+}
